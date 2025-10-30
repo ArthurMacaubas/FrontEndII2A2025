752930f9d61a38078609758d4a05f26d8aa023b5
Arthur Pietro <arthurpietro847@gmail.com>
2025-10-30 11:33:54 -0400

    30/10/2025

--- src/app/produtos/page.js
+++ src/app/produtos/page.js
@@ -1,6 +1,7 @@
-"use client"
+'use client';
 import { useState } from 'react';
 import styles from './page.module.css';
+import CardProduto from '@/components/Produto';
 
 export default function Produtos() {
     const produtos = [
@@ -16,20 +17,11 @@ export default function Produtos() {
         { id: 10, nome: "Mesa Digitalizadora", preco: 750 }
     ];
 
-<<<<<<< HEAD
-    const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    const pares = numeros.filter(num => (num % 2 === 0))
+    const [produto, setProduto] = useState(null);
 
-    const nomes = ["Arthur", "Julia", "Maria"]
-    const a = nomes.filter(name => (name.startsWith("A")))
-    const b = nomes.filter(name => (name.includes("u")))
-    const letra = "u"
-
-    const [busca, setBusca] = useState("")
-    const prodFilt = produtos.filter(prod => prod.nome.toLowerCase().includes(busca.toLowerCase()))
-
-    const [show, setShow] = useState(false)
-
+    const [busca, setBusca] = useState('');
+    const produtosFiltro = produtos.filter(prod => prod.nome.toLowerCase().includes(busca.toLowerCase()));
+    const [show, setShow] = useState(false);
     return (
         <div>
             <div className={styles.divTitulo}>
@@ -40,61 +32,40 @@ export default function Produtos() {
                     value={busca}
                 />
             </div>
-            {show && <div onClick={() => setShow(false)} className={styles.modal}>
-                
+            {produto && <div
+                className={styles.modal}
+                onClick={() => setProduto(null)}
+            >
+                <CardProduto
+                    nome={produto.nome}
+                    id={produto.id}
+                    preco={produto.preco}
+                />
             </div>}
-=======
-    return (
-        <div>
-            <h1>Produtos</h1>
->>>>>>> 24b6429613ba494b3ccb00e5ba27b15b44aef887
             <table className={styles.tbl}>
                 <thead>
                     <tr>
                         <th>ID</th>
-<<<<<<< HEAD
-                        <th>Nome</th>
-                        <th>Preço</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {prodFilt.map(produto => (
-                        <tr onClick={() => setShow(true)} key={produto.id}>
-                            <td>{produto.id}</td>
-                            <td>{produto.nome}</td>
-                            <td>{produto.preco.toLocaleString("pt-br", {
-                                style: "currency",
-                                currency: "BRL"
-=======
                         <th>NOME</th>
                         <th>PREÇO</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {produtos.map( p => (
-                        <tr key={p.id}>
+                    {produtosFiltro.map(p => (
+                        // ao clicar em uma linha da tabela, exiba uma div (modal) ocupando toda a viewport;
+                        // ao clicar na div, pare de exibir.
+                        <tr key={p.id} onClick={() => setProduto(p) // passar um, produto para a variável de estado
+                        }>
                             <td>{p.id}</td>
                             <td>{p.nome}</td>
                             <td>{p.preco.toLocaleString('pt-br', {
-                                style:'currency',
+                                style: 'currency',
                                 currency: 'BRL'
->>>>>>> 24b6429613ba494b3ccb00e5ba27b15b44aef887
                             })}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-<<<<<<< HEAD
-            <div className={styles.Ps}>
-                <p>Numeros pa.res: {pares}</p>
-                <p>Começam com A: {a}</p>
-                <p>Tem {letra} no nome: {b}</p>
-                <p>Filtrando por: </p>
-            </div>
-        </div>
-    )
-}
-=======
         </div>
     )
 }
@@ -111,4 +82,3 @@ export default function Produtos() {
 // Adicione um campo de texto (<input>) acima da tabela para permitir que o usuário digite o nome de um produto.
 /* Conforme o texto for digitado, mostre apenas os produtos cujo nome contenha o texto digitado (ignorando maiúsculas e minúsculas). */
 // Use o método filter() para aplicar o filtro e map() para exibir os resultados na tabela. 
->>>>>>> 24b6429613ba494b3ccb00e5ba27b15b44aef887
